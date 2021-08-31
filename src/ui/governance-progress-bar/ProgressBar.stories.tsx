4e9a2fd5f0468613932836bf62fba2eeb82460c8
IvanZen <32216147+IvanZen@users.noreply.github.com>
2021-08-31 15:20:24 +0800

    Fix/governance progress bar.stories.tsx (#50)
    * fix: [BOUNCE-86] text of comming pools (#2)
    
    * fix: [BOUNCE-86] text of comming pools
    
    * Update View.tsx
    
    Replaced '{in }' with '{in&nbsp}'
    
    * fix: removed timing-radio-field
    
    * fix: governance-progress-bar.stories.tsx

--- src/ui/governance-progress-bar/ProgressBar.stories.tsx
+++ src/ui/governance-progress-bar/ProgressBar.stories.tsx
@@ -1,11 +1,11 @@
-import { POOL_STATUS } from "@app/utils/pool";
+import { PROPOSAL_STATUS } from "@app/utils/governance";
 
 import { ProgressBar } from "./ProgressBar";
 
 export const Default = () => {
 	return (
 		<div style={{ width: 240 }}>
-			<ProgressBar status={POOL_STATUS.LIVE} fillInPercentage={30} />
+			<ProgressBar status={PROPOSAL_STATUS.LIVE} fillInPercentage={30} />
 		</div>
 	);
 };
@@ -13,15 +13,15 @@ export const Default = () => {
 export const Filled = () => {
 	return (
 		<div style={{ width: 240 }}>
-			<ProgressBar status={POOL_STATUS.FILLED} fillInPercentage={30} />
+			<ProgressBar status={PROPOSAL_STATUS.PASSED} fillInPercentage={30} />
 		</div>
 	);
 };
 
-export const Closed = () => {
+export const Failed = () => {
 	return (
 		<div style={{ width: 240 }}>
-			<ProgressBar status={POOL_STATUS.CLOSED} fillInPercentage={30} />
+			<ProgressBar status={PROPOSAL_STATUS.FAILED} fillInPercentage={30} />
 		</div>
 	);
 };
