aac18559ea980e0caeb76744b693110a7044712f
IvanZen <32216147+IvanZen@users.noreply.github.com>
2021-10-13 18:16:21 +0800

    Dev (#64)
    * 🐞 fix([proposalIndex]): arugment parsing
    
    * test
    
    * 🐞 fix:
    
    render all in governance

--- src/pages/governance/GovernanceView.tsx
+++ src/pages/governance/GovernanceView.tsx
@@ -15,6 +15,8 @@ import { Caption, Heading2, Body1 } from "@app/ui/typography";
 
 import { useMyTotalStaked, useProposalList } from "@app/web3/api/bounce/governance";
 
+import { ProposalDetail } from "../proposal-detail";
+
 import styles from "./Governance.module.scss";
 
 type GovernanceType = {
@@ -27,6 +29,26 @@ type GovernanceType = {
 	onSubmit?(values: any): any;
 };
 
+// console.log("window.location: ", window.location);
+
+function getUrlParam(name) {
+	const reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+
+	if (typeof window !== "undefined") {
+		const r = window.location.search.substr(1).match(reg);
+
+		if (r != null) return decodeURIComponent(r[2]);
+	}
+
+	return null;
+}
+
+const index = getUrlParam("proposalIndex");
+const id = getUrlParam("proposalId");
+
+console.log("index: ", index);
+console.log("id: ", id);
+
 export const GovernanceView: FC<GovernanceType & MaybeWithClassName> = ({ className }) => {
 	const myTotalStaked = useMyTotalStaked();
 	const proposalList = useProposalList();
@@ -34,57 +56,61 @@ export const GovernanceView: FC<GovernanceType & MaybeWithClassName> = ({ classN
 
 	return (
 		<>
-			<div className={classNames(className, styles.component)}>
-				<div className={styles.banner}>
-					<div className={styles.fakeTab}>
-						<Body1 className={styles.strVoting}>Voting</Body1>
-						<Button color="primary-white" onClick={() => routerPush(CREATE_PROPOSAL_PATH)}>
-							<Body1 className={styles.strCreateProposal}>+ Create Proposal</Body1>
-						</Button>
-					</div>
+			{index && id ? (
+				<ProposalDetail proposalIndex={+index} proposalId={id.toString()} />
+			) : (
+				<div className={classNames(className, styles.component)}>
+					<div className={styles.banner}>
+						<div className={styles.fakeTab}>
+							<Body1 className={styles.strVoting}>Voting</Body1>
+							<Button color="primary-white" onClick={() => routerPush(CREATE_PROPOSAL_PATH)}>
+								<Body1 className={styles.strCreateProposal}>+ Create Proposal</Body1>
+							</Button>
+						</div>
 
-					<div className={styles.bannerText}>
-						<Caption Component="span" className={styles.strVotePower} lighten={50}>
-							Your Voting Power:
-						</Caption>
-						<div className={styles.line2}>
-							<Heading2 className={styles.powerAmount}>
-								{myTotalStaked ? myTotalStaked : " "}
-							</Heading2>
-							&nbsp;
-							<Body1 className={styles.strVotes}>Votes</Body1>
+						<div className={styles.bannerText}>
+							<Caption Component="span" className={styles.strVotePower} lighten={50}>
+								Your Voting Power:
+							</Caption>
+							<div className={styles.line2}>
+								<Heading2 className={styles.powerAmount}>
+									{myTotalStaked ? myTotalStaked : " "}
+								</Heading2>
+								&nbsp;
+								<Body1 className={styles.strVotes}>Votes</Body1>
+							</div>
 						</div>
 					</div>
-				</div>
 
-				{proposalList && proposalList.length > 0 && (
-					<section className={styles.result}>
-						<GutterBox>
-							{proposalList && (
-								<ul className={styles.list}>
-									{proposalList.map((proposal, index) => (
-										<li key={proposal.index} className="animate__animated animate__flipInY">
-											<Card
-												content={proposal.content}
-												yesCount={proposal.yesCount}
-												noCount={proposal.noCount}
-												cancelCount={proposal.cancelCount}
-												creator={proposal.creator}
-												index={index.toString()}
-												status={proposal.status}
-												time={proposal.time}
-												title={proposal.title}
-												voteResult={proposal.voteResult}
-												href={`${GOVERNANCE_PATH}/${index}/${proposal.index}`}
-											/>
-										</li>
-									))}
-								</ul>
-							)}
-						</GutterBox>
-					</section>
-				)}
-			</div>
+					{proposalList && proposalList.length > 0 && (
+						<section className={styles.result}>
+							<GutterBox>
+								{proposalList && (
+									<ul className={styles.list}>
+										{proposalList.map((proposal, index) => (
+											<li key={proposal.index} className="animate__animated animate__flipInY">
+												<Card
+													content={proposal.content}
+													yesCount={proposal.yesCount}
+													noCount={proposal.noCount}
+													cancelCount={proposal.cancelCount}
+													creator={proposal.creator}
+													index={index.toString()}
+													status={proposal.status}
+													time={proposal.time}
+													title={proposal.title}
+													voteResult={proposal.voteResult}
+													href={`${GOVERNANCE_PATH}/${index}/${proposal.index}`}
+												/>
+											</li>
+										))}
+									</ul>
+								)}
+							</GutterBox>
+						</section>
+					)}
+				</div>
+			)}
 
 			<PopupTeleporterTarget />
 		</>
