a8d9dd750f965268c2249936d80d219c810d37fd
许敏慧 <homie_xu@192.168.10.11>
2021-10-21 19:14:56 +0800

    test rebuild

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withImages = require("next-images");
 
-module.exports = {
-	...withImages(),
-	distDir: "dist",
-	// trailingSlash: true,
-	// future: {
-	// 	webpack5: true,
-	// },
-};
+module.exports = withImages();
