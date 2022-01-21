733b6a9166cf71483f563884d1754c7a16c2ab66
LiJinLan <2254015596@qq.com>
2022-01-21 16:15:47 +0800

    fix: lbp list current price

---
+++ src/web3/const/volumeTokens.ts
@@ -0,0 +1,60 @@
+// 价值为$1的代币， 不用去查价格
+export const VolumeTokens = [
+	// USDC
+	{
+		name: "USDCoin",
+		address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+		symbol: "USDC",
+		decimals: 6,
+		chainId: 1,
+		logoURI:
+			"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png",
+	},
+	{
+		name: "USDCoin",
+		address: "0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b",
+		symbol: "USDC",
+		decimals: 6,
+		chainId: 4,
+		logoURI:
+			"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x4dbcdf9b62e891a7cec5a2568c3f4faf9e8abe2b/logo.png",
+	},
+
+	// DAI
+	{
+		name: "Dai Stablecoin",
+		address: "0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735",
+		symbol: "DAI",
+		decimals: 18,
+		chainId: 4,
+		logoURI:
+			"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735/logo.png",
+	},
+	{
+		name: "Dai Stablecoin",
+		address: "0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa",
+		symbol: "DAI",
+		decimals: 18,
+		chainId: 42,
+		logoURI:
+			"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa/logo.png",
+	},
+	{
+		name: "Dai Stablecoin",
+		address: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
+		symbol: "DAI",
+		decimals: 18,
+		chainId: 1,
+		logoURI:
+			"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png",
+	},
+	{
+		name: "Dai Stablecoin",
+		address: "0xaD6D458402F60fD3Bd25163575031ACDce07538D",
+		symbol: "DAI",
+		decimals: 18,
+		chainId: 3,
+		logoURI:
+			"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xaD6D458402F60fD3Bd25163575031ACDce07538D/logo.png",
+	},
+];
