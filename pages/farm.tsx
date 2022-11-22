e16f76fa0b27266e206dba683123760bbcf7b147
Ivan <32216147+IvanZen@users.noreply.github.com>
2022-11-22 18:08:37 +0800

    perf: add RequireConnectedWallet in farm page (#111)
    * feat: claiming slippage
    
    * fix: slippage tolerance style and slippage initialValue
    
    * perf: add RequireConnectedWallet in farm page

--- pages/farm.tsx
+++ pages/farm.tsx
@@ -1,10 +1,17 @@
+import { RequireConnectedWallet } from "@app/modules/require-connected-wallet";
 import { Layout } from "../src/layout";
 import NoSsr from "../src/modules/no-ssr/NoSsr";
 import { Farm } from "../src/pages/farm";
 import { pageWithLayout } from "../src/utils/pageInLayout";
 
 const Index = pageWithLayout(
-	() => <Farm />,
+	() => {
+		return (
+			<RequireConnectedWallet>
+				<Farm />
+			</RequireConnectedWallet>
+		);
+	},
 	({ children }) => (
 		<NoSsr>
 			<Layout title="" description="">
